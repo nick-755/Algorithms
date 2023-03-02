d540f0ea49b917d98ef9fa94bb6b6963a13b1c4f
Illllp <lukasjohancen@yandex.ru>
2023-03-02 12:36:33 +0300

    Using linked lists

---
+++ 16.py
@@ -0,0 +1,33 @@
+'''
+23. Дан список, содержащий запись неотрицательных целых чисел в двоичной системе счисления.
+Заменить каждый элемент списка на его запись в шестнадцатеричной системе счисления. 
+'''
+# conversion from binary to hexadecimal
+
+
+a = ['10000', '11001', '100']
+
+# An important class for specifying the beginning and end
+
+
+class Node:
+
+    def __init__(self, data):
+
+        self.next = None
+        self.data = data
+
+    def binToHex(self, bin):
+
+        for i in range(len(bin)):
+
+            num = int(bin[i], 2)  # We point the program to the binary system
+            hex_num = format(num, 'x')  # Specify the translation format
+            bin[i] = hex_num  # Output the hexadecimal system alternately
+
+
+ll = Node(a)
+ll.binToHex(a)
+
+
+print(ll.data)
